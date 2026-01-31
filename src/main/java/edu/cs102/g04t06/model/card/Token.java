6580a4255b216cac890b076bc86b5491d009e0ae
Wei Rong <developer.swr.2001@gmail.com>
2026-01-31 14:04:28 +0800

    Add reorganized model files

---
+++ src/main/java/edu/cs102/g04t06/model/card/Token.java
@@ -0,0 +1,13 @@
+package edu.cs102.g04t06.model;
+
+public class Token {
+    private Type.TokenType tokenType;
+
+    public Type.TokenType getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(Type.TokenType tokenType) {
+        this.tokenType = tokenType;
+    }
+}
