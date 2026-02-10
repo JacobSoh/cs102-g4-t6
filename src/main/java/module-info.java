f254fb8a99c2fd4244d33d2a09e103289aebae72
Wei Rong <developer.swr.2001@gmail.com>
2026-02-10 11:13:19 +0800

    Shifted old view model to presentation layer

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,15 @@
-/**
- * Core module for the Splendor JavaFX application.
- */
 module edu.cs102.g04t06 {
     requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.base;
+    
     exports edu.cs102.g04t06;
-    exports edu.cs102.g04t06.view;
+    exports edu.cs102.g04t06.game.execution;
+    exports edu.cs102.g04t06.game.execution.ai;
+    exports edu.cs102.g04t06.game.infrastructure.config;
+    exports edu.cs102.g04t06.game.infrastructure.data;
+    exports edu.cs102.g04t06.game.presentation.console;
+    exports edu.cs102.g04t06.game.rules;
+    exports edu.cs102.g04t06.game.rules.entities;
+    exports edu.cs102.g04t06.game.rules.valueobjects;
 }
