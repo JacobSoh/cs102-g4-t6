84cabb922a7f7cbbea2eb327c2535482230235fd
Chen Mingda <chenmingda56@gmail.com>
2026-02-12 02:16:02 +0800

    Added GemCollection and Enum GemColor

--- src/main/java/edu/cs102/g04t06/game/rules/entities/GemColor.java
+++ src/main/java/edu/cs102/g04t06/game/rules/entities/GemColor.java
@@ -1,2 +1,14 @@
 package edu.cs102.g04t06.game.rules.entities;
 
+/**
+ * Represents the different gem colors in the Splendor game.
+ * GOLD is a wild card gem obtained by reserving cards.
+ */
+public enum GemColor {
+    WHITE,
+    BLUE,
+    GREEN,
+    RED,
+    BLACK,
+    GOLD
+}
